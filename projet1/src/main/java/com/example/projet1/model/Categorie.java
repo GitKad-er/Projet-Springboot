5214e9c512e1922bba552e4eed92799e363f5447
Kader <kaderlema01@gmail.com>
2022-04-28 02:25:44 +0000

    V2.2.1

--- projet1/src/main/java/com/example/projet1/model/Categorie.java
+++ projet1/src/main/java/com/example/projet1/model/Categorie.java
@@ -3,6 +3,7 @@ package com.example.projet1.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
 @Entity
 @AllArgsConstructor
 @Data
+@ToString(exclude = "produits")
 @NoArgsConstructor
 public class Categorie {
 
